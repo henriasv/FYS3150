c4f75cf98103cb2934f9da18e84b30d7a012fdfa
Henrik Sveinsson <henrik.sveinsson@me.com>
2012-09-12 00:50:47 +0200

    first upload of code

---
+++ project1/src/project1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// defining arrays
+	N = 10;
+	a = new double[N];
+	b = new double[N];
+	c = new double[N];
+	
+	
+}
+
+void forwardSubstitution(double &a, double &b, int N) {
+	for (int i = 0; i<N; i++) {
+		
+	}
+}
