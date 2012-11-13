e4036e61bd03add3e17ee128ab671aadc17c08c6
Henrik Andersen Sveinsson <henriasv@robinja.uio.no>
2012-11-13 23:52:18 +0100

    started programming the ising model

---
+++ IsingModel2D/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: henriasv
+ *
+ * Created on 8. november 2012, 16:37
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
